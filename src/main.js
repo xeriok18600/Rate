680c361164c5b235c0ba07f8a11edcc598347f29
admin <admin@example.com>
2019-01-23 18:20:27 +0800

    Add Chart.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './plugins/vuetify'
 import router from './router'
 
 
-
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,7 +1,66 @@
 <template lang="pug">
-h1 Wait ....
+v-layout
+    v-flex(xs12='', sm6='')
+        v-card
+            v-card-title
+                h3.headline.mb-0 現金買入
+            canvas#myChart
 </template>
 
+
 <script>
-export default {};
+import Chart from "chart.js";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: [
+          "一月",
+          "二月",
+          "三月",
+          "四月",
+          "五月",
+          "六月",
+          "七月",
+          "八月",
+          "九月",
+          "十月",
+          "十一月",
+          "十二月"
+        ],
+        datasets: [
+          {
+            borderColor: ["rgba(255,99,132,1)"],
+            borderWidth: 1,
+            label: "美金 (USD)",
+            data: [12, 19, 3, 5, 2, 3]
+          },
+          {
+            label: "港幣 (HKD)",
+            data: [22, 29, 31, 5, 8, 3]
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+</style>
